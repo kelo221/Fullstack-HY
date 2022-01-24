89eacdab3ad1a044bea0c2848a480c5bcd014292
kelo221 <61495413+kelo221@users.noreply.github.com>
2022-01-24 19:12:46 +0200

    part01

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+function Part(props) {
+
+    return (<p>
+        {props.name} {props.count}
+    </p>);
+}
+
+function Content(props) {
+
+    console.log(props)
+
+    return (
+        <div>
+            <Part name={props.parts[0].name} count={props.parts[0].exercises}/>
+            <Part name={props.parts[1].name} count={props.parts[1].exercises}/>
+            <Part name={props.parts[2].name} count={props.parts[2].exercises}/>
+        </div>
+    )
+}
+
+function Total(props) {
+    return (<p>Number of exercises {props.total}</p>);
+}
+
+function Header(props) {
+
+    return (<h1>{props.course.name}</h1>);
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+/*    console.log(course.name)
+    console.log(course.parts[0].name)
+    console.log(course.parts[0].exercises)*/
+
+    return (
+        <div>
+            <Header course={course}/>
+            <Content parts={course.parts}
+            />
+            <Total
+                total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+            />
+        </div>
+    )
+}
+
+export default App
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,118 @@
+import React, {useState} from 'react'
+
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+
+/*function CountReturner({counter, name}) {
+
+    if (!isNaN(counter)) {
+
+        if (name === "of positive") {
+            return (<p> {counter} {name} %</p>)
+        } else
+            return (<p> {counter} {name} </p>)
+
+    } else {
+        return (<p>{0} {name}</p>)
+    }
+}*/
+
+function StatisticLine({text, value}) {
+    return (
+        <>
+
+            <tr>
+                <td>{text}</td>
+                <td>{value}</td>
+            </tr>
+
+        </>
+//        <p> {value} {text} </p>
+
+
+    )
+}
+
+function Statistics({pos, neg, neu, total, avgTotal, avgPos}) {
+
+    if (total !== 0) {
+        return (
+            <>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Option</th>
+                        <th>Value</th>
+                    </tr>
+
+                    <StatisticLine text="positive" value={pos}/>
+                    <StatisticLine text="neutral" value={neu}/>
+                    <StatisticLine text="negative" value={neg}/>
+                    <StatisticLine text="avgTotal" value={avgTotal}/>
+                    <StatisticLine text="avgPos" value={avgPos}/>
+                    </tbody>
+                </table>
+            </>
+        )
+    } else {
+        return (<p>NO DATA</p>)
+    }
+}
+
+const App = () => {
+    const [positive, incrementPos] = useState(0)
+    const [neutral, incrementNeu] = useState(0)
+    const [negative, incrementNeg] = useState(0)
+
+    const increasePosByOne = () => incrementPos(positive + 1)
+    const increaseNeuByOne = () => incrementNeu(neutral + 1)
+    const increaseNegByOne = () => incrementNeg(negative + 1)
+
+    const total = positive + negative + neutral
+    const avgPos = positive / total * 100
+    const avgTotal = (positive - negative) / total
+
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+
+            <Button
+                onClick={increasePosByOne}
+                text='positive'
+            />
+            <Button
+                onClick={increaseNeuByOne}
+                text='neutral'
+            />
+            <Button
+                onClick={increaseNegByOne}
+                text='negative'
+            />
+            <h1>Statistics</h1>
+
+            {/*            <CountReturner counter={positive} name="positive"/>
+
+            <CountReturner counter={neutral} name="neutral"/>
+
+            <CountReturner counter={negative} name="negative"/>
+
+            <CountReturner counter={total} name="total"/>
+
+            <CountReturner counter={avgTotal} name="avgTotal"/>
+
+            <CountReturner counter={avgPos} name="of positive"/>*/}
+
+            <Statistics pos={positive} neg={negative} neu={neutral} total={total} avgTotal={avgTotal}
+                        avgPos={avgPos}/>
+
+        </div>
+    )
+}
+
+export default App
